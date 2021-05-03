9b87837ef19f8ba0ff2daf0db4d49ef440f27308
GabrielRhuan <74512499+GabrielRhuan@users.noreply.github.com>
2021-05-03 10:15:50 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Como era a composição do clero?",
+    answer: "Cardeais,bispos e abades no alto clero e, padres,frades e monges no baixo clero",
+    options: [
+      "Cardeais, reis e abades",
+      "Cardeais, rainha e abades no alto clero e, padres,frades e monges no baixo clero",
+      "padres,frades e monges",
+      "Cardeais,bispos e abades no alto clero e, padres,frades e monges no baixo clero"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quando foi a queda da Bastilha?",
+    answer: "14 de junho de 1789",
+    options: [
+      "14 de junho de 1789",
+      "14 de abril de 1779",
+      "14 de agosto de 1789",
+      "14 de julho de 1799"
+    ]
+  },
+    {
+    numb: 3,
+    question: "O que foi o voto censitário?",
+    answer: "Voto baseado na renda onde só os proprietários pagantes de impostos podiam votar",
+    options: [
+      "Voto baseado na idade onde só os quem tinha acima de 18 anos podiam votar",
+      "Voto baseado na renda onde só os proprietários pagantes de impostos podiam votar",
+      "Voto baseado na renda onde só quem era de família com baixa renda podiam votar",
+      "Voto baseado na religião onde só quem era cristão podiam votar"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Como a sociedade francesa estava dividida?",
+    answer: "Clero, nobreza e camponeses",
+    options: [
+      "Burgueses, nobreza e camponeses",
+      "Políticos, nobreza e camponeses",
+      "Clero, nobreza e camponeses",
+      "Igreja, burgueses e escravos"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A nobreza vivia a custo de quê?",
+    answer: "Vivia à custa do Estado ou da exploração do trabalho dos camponeses.",
+    options: [
+      "Vivia das vendas do trigo.",
+      "Vivia à custa do Rei.",
+      "Vivia à custa do empréstimo de dinheiro de outros países.",
+      "Vivia à custa do Estado ou da exploração do trabalho dos camponeses."
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
